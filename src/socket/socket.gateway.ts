bfcf71e7b3a246a0239c8113b4e770de663c7b6c
xyb <xingyibiao@live.com>
2019-11-02 12:25:53 +0800

    feat: 第一次提交

---
+++ src/socket/socket.gateway.ts
@@ -0,0 +1,98 @@
+import { SubscribeMessage, WebSocketGateway, WebSocketServer, OnGatewayConnection, OnGatewayDisconnect } from '@nestjs/websockets';
+import { Socket, Server } from 'socket.io';
+
+type UserInfo = {
+  sessionId: string,
+  userName: string,
+  roomName: string,
+}
+
+@WebSocketGateway({ transports: ['websocket'] })
+export class SocketGateway implements OnGatewayConnection, OnGatewayDisconnect {
+  @WebSocketServer()
+  private server: Server;
+
+  private roomMap: Map<string, UserInfo[]> = new Map();
+
+  handleConnection(client: Socket) {
+    console.log('加入网关', client.id)
+  }
+
+  handleDisconnect(client: Socket) {
+    console.log('离开网关', client.id)
+    this.roomMap.forEach((room, key) => {
+      room.forEach((user, i) => {
+        if (user.sessionId === client.id) {
+          const users = this.roomMap.get(key)
+          this.roomMap.set(key, users.splice(i, 1))
+          console.log(this.roomMap)
+        }
+      })
+    })
+  }
+
+  private getUserInfoBySId(sessionId: string) {
+    let sender: string;
+    let roomName: string;
+    this.roomMap.forEach((room, key) => {
+      room.forEach((user) => {
+        if (user.sessionId === sessionId) {
+          sender = user.userName;
+          roomName = key;
+        }
+      })
+    })
+
+    return {
+      sender,
+      roomName,
+    }
+  }
+  
+  @SubscribeMessage('message')
+  handleMessage(client: any, payload: any): string {
+    return 'Hello world!';
+  }
+
+  @SubscribeMessage('login')
+  handlerLogin(client: Socket, payload: any): boolean {
+    const [userName, roomName] = payload;
+    try {
+      client.join(roomName);
+      const userRoom = this.roomMap.get(roomName);
+      const isPublisher = Array.isArray(userRoom) && userRoom.length > 0;
+      const room = this.roomMap.get(roomName)
+      const user: UserInfo = {
+        sessionId: client.id,
+        userName,
+        roomName,
+      }
+      if (room) {
+        room.push(user)
+      } else {
+        this.roomMap.set(roomName, [user])
+      }
+      console.log(isPublisher);
+      return isPublisher;
+      
+    } catch(e) {
+      console.error(e)
+    }
+  }
+
+  @SubscribeMessage('send_sdp')
+  handlerSdpMsg(client: Socket, data: any) {
+    const { sender, roomName } = this.getUserInfoBySId(client.id);
+
+    if (!sender) return;
+    this.server.in(roomName).emit('send_sdp', { data, sender });
+  }
+
+  @SubscribeMessage('candidate')
+  handlerCandidateMsg(client: Socket, data: any) {
+    const { sender, roomName } = this.getUserInfoBySId(client.id);
+
+    if (!sender) return;
+    this.server.in(roomName).emit('candidate', { data, sender });
+  }
+}
