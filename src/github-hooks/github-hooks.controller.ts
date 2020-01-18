ebf1e8a44e0d62328d1d9687e4621e56dac96fff
xyb <xingyibiao@live.com>
2020-01-18 17:03:31 +0800

    feat: github webhooks 第一次提交

--- src/github-hooks/github-hooks.controller.ts
+++ src/github-hooks/github-hooks.controller.ts
@@ -1,4 +1,11 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Controller, Get, Post, Req, Response } from '@nestjs/common';
+import { createHmac } from 'crypto';
+import bl  = require('bl');
+import * as path from 'path';
+import { spawn } from 'child_process';
+
+const SECRET = 'gayhub';
+const SHELL_PATH = path.join('/root/blog', 'deploy.sh');
 
 @Controller('github-hooks')
 export class GithubHooksController {
@@ -8,7 +15,73 @@ export class GithubHooksController {
   }
 
   @Post('/')
-  handlerWebHooks() {
+  handlerWebHooks(@Req() req, @Response() res) {
     // TODO impl it
+    const { headers } = req;
+    const delivery = headers['x-github-delivery'];
+    const signature = headers['x-hub-signature'];
+    const event = headers['x-github-event'];
+    const ua = headers['user-agent'];
+    if (!delivery || !signature || !event || !ua.includes('GitHub-Hookshot')) {
+      return this.handlerError(res, 'not Auth');
+    }
+
+    const [signType, signData] = signature.split('=');
+    req.pipe(new bl((error, data) => {
+      if (error) {
+        return this.handlerError(res, 'server error');
+      }
+
+      if (signData !== this.sign(signType, data, SECRET)) {
+        return this.handlerError(res, 'not Auth');
+      }
+
+      res.writeHead(200, { 'content-type': 'application/json' });
+      res.send({ ok: true });
+
+      if (event === 'push') {
+        this.deploy(req.body.repository);
+      }
+    }));
+    // if (signData === this.sign(signType, ))
+  }
+
+  private handlerError(@Response() res, msg: string) {
+    res.status(400);
+    res.send(msg);
+  }
+
+  private sign(type: string, data: Buffer, secret: string) {
+    return createHmac(type, secret).update(data).digest('hex');
+  }
+
+  private deploy(repository: string) {
+    if (!repository) {
+      return;
+    }
+    const [userName, repositoryName] = repository.split('/');
+    if (repositoryName === 'blog') {
+      console.log('deloy blog start');
+      this.deployBlog();
+    }
+  }
+
+  private deployBlog() {
+    this.runCmd('sh', [SHELL_PATH]);
+  }
+
+  private runCmd(cmd: string, args: string[]) {
+    return new Promise((resolve, reject) => {
+      const shell = spawn(cmd, args);
+      shell.stderr.on('data', (e) => console.error(e));
+      shell.on('close', (code) => {
+        if (code !== 0) {
+          console.error(`${cmd} ${args} error, code: ${code}`);
+          reject(code);
+          return;
+        }
+        resolve(code);
+      });
+    });
   }
 }
