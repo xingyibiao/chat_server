c8b15af3e33ae786e4bf5ae2288137729293a8ad
xyb <xingyibiao@live.com>
2020-01-18 15:06:57 +0800

    feat: test new controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SocketModule } from './socket/socket.module';
+import { GithubHooksController } from './github-hooks/github-hooks.controller';
 
 @Module({
   imports: [SocketModule],
-  controllers: [AppController],
+  controllers: [AppController, GithubHooksController],
   providers: [AppService],
 })
 export class AppModule {}
